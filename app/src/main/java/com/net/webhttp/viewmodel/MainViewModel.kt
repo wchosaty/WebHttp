9d9b04a22585216a6ea0937fb46d9e09f0a37a8b
W.Allen <whosaty@gmail.com>
2023-04-02 11:21:18 +0800

    add fun toWeb in viewModel

--- app/src/main/java/com/net/webhttp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/net/webhttp/viewmodel/MainViewModel.kt
@@ -2,9 +2,38 @@ package com.net.webhttp.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.gson.JsonObject
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainViewModel: ViewModel() {
     val numberA :MutableLiveData<String> by lazy { MutableLiveData() }
     val numberB : MutableLiveData<String> by lazy { MutableLiveData() }
     val result: MutableLiveData<String> by lazy { MutableLiveData() }
+    val servletURL = "http://10.0.2.2:8080/CoroutineGson/GsonTest"
+
+    suspend fun toWebCaculator() {
+        var j = JsonObject()
+        j.addProperty("a",numberA.value.toString().trim())
+        j.addProperty("b",numberB.value.toString().trim())
+        ( URL(servletURL).openConnection() as HttpURLConnection ).run {
+            doInput = true
+            doOutput = true
+            requestMethod = "POST"
+            useCaches = false
+            setChunkedStreamingMode(0)
+            setRequestProperty("charset","UTF-8")
+            outputStream.bufferedWriter().use {
+                it.write(j.toString())
+            }
+            if(responseCode == 200) {
+                val backString = inputStream.bufferedReader().readLine()
+                withContext(Dispatchers.Main) {
+                    result.value = backString.trim()
+                }
+            }
+        }
+    }
 }

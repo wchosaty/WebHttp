c2b2063e5590955a7edf95e315d7f69261ae8244
W.Allen <whosaty@gmail.com>
2023-04-02 09:24:56 +0800

    Create New

---
+++ app/src/main/java/com/net/webhttp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.net.webhttp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

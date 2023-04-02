3dbc9a2ba90f7439b7faf426d72279fd91ad9405
W.Allen <whosaty@gmail.com>
2023-04-02 12:42:07 +0800

    add MainActivity binding use CoroutineScope in MainViewModel

--- app/src/main/java/com/net/webhttp/MainActivity.kt
+++ app/src/main/java/com/net/webhttp/MainActivity.kt
@@ -2,10 +2,23 @@ package com.net.webhttp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModel
+import com.google.gson.annotations.Until
+import com.net.webhttp.databinding.ActivityMainBinding
+import com.net.webhttp.viewmodel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val viewModel : MainViewModel by viewModels()
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+
     }
 }
--- app/src/main/java/com/net/webhttp/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/net/webhttp/viewmodel/MainViewModel.kt
@@ -1,39 +1,51 @@
 package com.net.webhttp.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.google.gson.JsonObject
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.net.HttpURLConnection
 import java.net.URL
 
 class MainViewModel: ViewModel() {
+    val TAG = "TAG ${javaClass.simpleName}"
     val numberA :MutableLiveData<String> by lazy { MutableLiveData() }
     val numberB : MutableLiveData<String> by lazy { MutableLiveData() }
     val result: MutableLiveData<String> by lazy { MutableLiveData() }
     val servletURL = "http://10.0.2.2:8080/CoroutineGson/GsonTest"
 
-    suspend fun toWebCaculator() {
+    fun toWebCaculator() {
+        Log.d(TAG,"onClick")
         var j = JsonObject()
-        j.addProperty("a",numberA.value.toString().trim())
-        j.addProperty("b",numberB.value.toString().trim())
-        ( URL(servletURL).openConnection() as HttpURLConnection ).run {
-            doInput = true
-            doOutput = true
-            requestMethod = "POST"
-            useCaches = false
-            setChunkedStreamingMode(0)
-            setRequestProperty("charset","UTF-8")
-            outputStream.bufferedWriter().use {
-                it.write(j.toString())
-            }
-            if(responseCode == 200) {
-                val backString = inputStream.bufferedReader().readLine()
-                withContext(Dispatchers.Main) {
-                    result.value = backString.trim()
-                }
+        j.addProperty("a", numberA.value.toString().trim())
+        j.addProperty("b", numberB.value.toString().trim())
+        Log.d(TAG,"toWebCaculator :" + j.toString())
+        CoroutineScope(Dispatchers.IO).launch {
+//            load(j.toString())
+            (URL(servletURL).openConnection() as HttpURLConnection).run {
+                doInput = true
+                doOutput = true
+                requestMethod = "POST"
+                useCaches = false
+                setChunkedStreamingMode(0)
+                setRequestProperty("charset", "UTF-8")
+                Log.d(TAG, "viewModelScope")
+                    outputStream.bufferedWriter().use {
+                        it.write(j.toString())
+                    }
+                    if (responseCode == 200) {
+                        val backString = inputStream.bufferedReader().readLine()
+                        withContext(Dispatchers.Main){
+                            result.value = backString.trim()
+                        }
+                    }
             }
         }
     }
+
 }
